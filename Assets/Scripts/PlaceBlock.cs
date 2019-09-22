7ef8e1591e3f92007aa13138a9166844c38947c5
Thomas Roberts <temersus@gmail.com>
2019-09-22 12:12:55 -0700

    Fixed extra highlighting issue

--- Assets/Scripts/PlaceBlock.cs
+++ Assets/Scripts/PlaceBlock.cs
@@ -28,8 +28,9 @@ public class PlaceBlock : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         var block = other.gameObject;
-        float blockSize = block.transform.lossyScale.x;
-        Vector3 normal = GetNormalVector(block) * blockSize;
+        var normal = GetNormalVector(block);
+
+        Destroy(highlighterBlock);
         highlighterBlock = Instantiate(highlighterPrefab, other.transform.position + normal, Quaternion.identity);
     }
 
@@ -40,20 +41,24 @@ public class PlaceBlock : MonoBehaviour
 
     private Vector3 GetNormalVector(GameObject other)
     {
+        //vector3 of path from hand's position to other gameobject's position
         var pathToBlock = transform.position - other.transform.position;
+
+        //Gets magnitude of components.
         float xComponent = pathToBlock.x * pathToBlock.x;
         float yComponent = pathToBlock.y * pathToBlock.y;
         float zComponent = pathToBlock.z * pathToBlock.z;
 
+        //Checks which component is greatest, and returns a new vector3 of ONLY that component of pathToBlock.
         if (xComponent > yComponent && xComponent > zComponent)
         {
-            return Vector3.left;
+            return new Vector3(pathToBlock.x, 0, 0);
         }
         if (yComponent > xComponent && yComponent > zComponent)
         {
-            return Vector3.up;
+            return new Vector3(0, pathToBlock.y, 0);
         }
-        return Vector3.forward;
+        return new Vector3 (0, 0, pathToBlock.z);
     }
 }
 
