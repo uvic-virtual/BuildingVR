6ee9f177407fd02b1f3d93fae3a5fea7ac5f0392
deanroland <45372296+deanroland@users.noreply.github.com>
2019-09-27 15:41:05 -0700

    imported alex's garbage

---
+++ V4.0.1/Assets/Scripts/1.Intro/OnClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour
+{
+    //Access function from the FadeIn.cs
+    private FadeIn Fader;
+    //Check if it's first time clicking the button
+    private bool Clicked;
+    //Sound effect
+    public AudioSource SoundEffect;
+
+    void Start()
+    {
+        Fader = GameObject.FindObjectOfType<FadeIn>();
+        Clicked = false;
+    }
+
+    public void OnClickFunction()
+    {
+        //Do transition only once
+        if (!Clicked)
+        {
+            SoundEffect.Play();
+            Clicked = true;
+            Fader.FadeBlack();
+        }
+    }
+}
---
+++ V4.0.1/Assets/Scripts/3.Pet/health/IHealable.cs
@@ -0,0 +1,16 @@
+﻿namespace Health
+{
+    /// <summary>
+    /// Used to restore health.</summary>
+    public interface IHealable
+    {
+        /// <summary>
+        /// Current health of Entity.</summary>
+        int Health { get; }
+        
+        /// <summary>
+        /// Adds specified amount of health.</summary>
+        /// <param name="health">Amount of health added.</param>
+        void AddHealth(int health);
+    }
+}
