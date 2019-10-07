095a128d5dddce0121137515afc38796fed8fb1d
= <temersus@gmail.com>
2019-10-06 22:17:48 -0700

    Imported zombie scripts (note to self: fix these)

---
+++ NonVR/Assets/Scripts/Zombie/AttackPlayer.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Used by Attack Zombie to attack player.</summary>
+public class AttackPlayer : MonoBehaviour
+{
+    /// <summary>
+    /// How close the zombie has to be to attack the player.</summary>
+    [SerializeField] private float AttackRadius = 2.0f;
+
+    /// <summary>
+    /// Time between iterations in DamagePlayer routine.</summary>
+    [SerializeField] private float AttackTimeDelay = 0.5f;
+
+    /// <summary>
+    /// Health taken off player in DamagePlayer routine.</summary>
+    [SerializeField] private int DamagePerDelay = 1;
+
+    /// <summary>
+    /// FollowTarget component of zombie.</summary>
+    private FollowTarget movement;
+
+    /// <summary>
+    /// What do you honestly think?</summary>
+    private static GameObject player;
+
+    /// <summary>
+    /// Damage interface of player's HealthManager component.</summary>
+    private static IDamageable playerHealth;
+
+    //Keeps track of damage coroutine.
+    private Coroutine damageRoutine;
+    private bool damageRoutineRunning;
+
+	private void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            playerHealth = player.GetComponentInChildren<IDamageable>();
+        }
+
+        //set target to player.
+        movement = GetComponent<FollowTarget>();
+        movement.Target = player;
+	}
+
+    /// <summary>
+    /// Checks if close to player and does stuff accordingly.</summary>
+    private void Update()
+    {
+        /* 
+         * Logic of this:
+         *  if close to player:
+         *      Look at player
+         *      Stop moving
+         *      Damage Player
+         *  if not close to player:
+         *      Start Moving 
+         *      Stop Damaging 
+         */
+        //bool closeToPlayer = Vector3.Distance(transform.position, player.transform.position) < AttackRadius;
+        //if (closeToPlayer)
+        //{
+        //    movement.FaceTarget();
+        //    movement.CurrentlyMoving = false;
+
+        //    if (!damageRoutineRunning)
+        //    {
+        //        damageRoutine = StartCoroutine(DamagePlayer(DamagePerDelay, AttackTimeDelay));
+        //    }
+        //}
+        //else //if not close to player:
+        //{
+        //    movement.CurrentlyMoving = true;
+
+        //    if (damageRoutineRunning)
+        //    {
+        //        StopCoroutine(damageRoutine);
+        //        damageRoutineRunning = false;
+        //    }
+        //}
+    }
+
+    /// <summary>
+    /// Takes off an amount of health per timed iteration.
+    /// </summary>
+    /// <param name="damagePerDelay">Amount of damage taken off per iteration.</param>
+    /// <param name="timeDelay">Time between iterations.</param>
+    /// <returns></returns>
+    private IEnumerator DamagePlayer(int damagePerDelay, float timeDelay)
+    {
+        damageRoutineRunning = true;
+  
+        while (playerHealth.Health > 0)
+        {
+            playerHealth.Hit(damagePerDelay);
+            Debug.Log(playerHealth.Health);
+            yield return new WaitForSeconds(timeDelay);
+        }
+
+        damageRoutineRunning = false;
+    }
+}
---
+++ NonVR/Assets/Scripts/Zombie/StealRelic.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StealRelic : MonoBehaviour
+{
+    /// <summary>
+    /// How high relic is carried above zombie.  Prevents clipping.</summary>
+    [SerializeField] private float RelicHeightDisplacement = 1.8f;
+
+    /// <summary>
+    /// Delegate for relic events.</summary>
+    private delegate void RelicDelegate ();
+
+    /// <summary>
+    /// Static event, called when thing happens.  Used to change targets other zombies are following. </summary>
+    private static event RelicDelegate RelicPickedUp, RelicDropped;
+
+    /// <summary>
+    /// Potential destination of zombie.</summary>
+    private static GameObject relic, temple;
+
+    /// <summary>
+    /// gameobject currently being carried by zombie. </summary>
+    private GameObject carriedObject;
+
+    /// <summary>
+    /// Health component of zombie. DropObject added to it's death event to prevent relic dissappearing when a zombie is killed.</summary>
+    private HealthManager health;
+
+    /// <summary>
+    /// FollowTarget component of this zombie.</summary>
+    private FollowTarget movement;
+
+    private void Start()
+    {
+        if (relic == null)
+        {
+            relic = GameObject.FindGameObjectWithTag("Relic");
+        }
+
+        if (temple == null)
+        {
+            temple = GameObject.FindGameObjectWithTag("Temple");
+        }
+        
+        health = GetComponent<HealthManager>();
+        movement = GetComponent<FollowTarget>();
+
+      
+        //Add delegates to events.
+        health.Death += DropObject;
+        RelicPickedUp += GotoTemple;
+        RelicDropped += GotoRelic;
+
+        //Set target to relic.
+        movement.Target = relic;
+    }
+
+    /// <summary>
+    /// Cleans up events when zombie is destroyed. </summary>
+    private void OnDestroy()
+    {
+        health.Death -= DropObject;
+        RelicPickedUp -= GotoTemple;
+        RelicDropped -= GotoRelic;
+    }
+
+    /// <summary>
+    /// Drops object being carried by this zombie.
+    /// Called in health.Death to prevent relic dissapearing when zombie dies.</summary>
+    private void DropObject()
+    {
+        if (carriedObject != null)
+        {
+            Rigidbody rigidbody = carriedObject.GetComponent<Rigidbody>();
+            rigidbody.isKinematic = true;
+
+            carriedObject.transform.parent = null;
+            carriedObject = null;
+
+            RelicDropped();
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.Equals(relic))
+        {
+            PickupObject(relic);
+        }
+    }
+
+
+    /// <summary>
+    /// Picks up specified gameobject.
+    /// (Makes it a child of the zombie and places it above zombie).
+    /// </summary>
+    /// <param name="other">Gameobject to pickup.</param>
+    private void PickupObject(GameObject other)
+    {
+        carriedObject = other;
+        carriedObject.transform.parent = transform;
+        carriedObject.transform.localPosition = Vector3.up * RelicHeightDisplacement;
+        carriedObject.transform.localRotation = Quaternion.identity;
+
+        Rigidbody rigidbody = carriedObject.GetComponentInChildren<Rigidbody>();
+        rigidbody.isKinematic = true;
+
+        RelicPickedUp();
+    }
+
+    //TODO: Make Goto(temple/relic) one method, figure out how event thing with parameters work.
+    
+    /// <summary>
+    /// Changes target to temple.</summary>
+    private void GotoTemple()
+    {
+        movement.Target = temple;
+    }
+
+    /// <summary>
+    /// Changes target to relic.
+    /// Called in RelicDropped when a relic is dropped.</summary>
+    private void GotoRelic()
+    {
+        movement.Target = relic;
+    }
+}
