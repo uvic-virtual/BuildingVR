361ee90ebc04e4ab65539ffdf5ec41775e87f154
deanroland <45372296+deanroland@users.noreply.github.com>
2019-10-18 13:42:21 -0700

    made unityevent serialized private

--- NonVR/Assets/Scripts/Health/HealthManager.cs
+++ NonVR/Assets/Scripts/Health/HealthManager.cs
@@ -9,7 +9,7 @@ public class HealthManager : MonoBehaviour, IDamageable
     [SerializeField] private int MaxHealth = 100; //pretend this is a constant.
 
     //Death event (stuff you want to happen when this zombie dies).
-    [SerializeField] private UnityEvent Death;
+    [SerializeField] public UnityEvent Death;
 
     /// <summary>
     /// Health bar graphic that fills/shrinks depending on health.</summary>
