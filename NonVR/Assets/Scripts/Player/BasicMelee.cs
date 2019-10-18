4c0d5852ca649dd5cf6c25065f51d748efdfc5b2
deanroland <45372296+deanroland@users.noreply.github.com>
2019-10-18 13:21:48 -0700

    Re-created test fighting script

---
+++ NonVR/Assets/Scripts/Player/BasicMelee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class BasicMelee : MonoBehaviour
+{
+    [SerializeField] private int DamagePerHit = 50;
+
+    private IDamageable CachedDamage;
+
+    private GameObject CachedGameObject;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.Equals(CachedGameObject))
+        {
+            CachedGameObject = other.gameObject;
+
+            try
+            {
+                CachedDamage = CachedGameObject.transform.parent.GetComponentInChildren<IDamageable>();
+            }
+            catch (NullReferenceException)
+            {
+                CachedDamage = CachedGameObject.GetComponentInChildren<IDamageable>();
+            }
+        }
+
+        if (CachedDamage != null)
+        {
+            CachedDamage.Health -= DamagePerHit;
+            Debug.Log(CachedDamage.Health);
+        }
+    }
+}
