973eefc5f4c8f9360cf112e18f5e8a6d81b67961
= <temersus@gmail.com>
2019-10-17 18:49:16 -0700

    Added test event script

---
+++ NonVR/Assets/Scripts/Bleah.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Bleah : MonoBehaviour
+{
+    public void PublicPrint()
+    {
+        Debug.Log("this is the public one");
+    }
+
+    private void PrivatePrint()
+    {
+        Debug.Log("this is the private one");
+    }
+}
